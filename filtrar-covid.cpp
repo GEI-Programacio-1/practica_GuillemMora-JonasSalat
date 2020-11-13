8e734d3e1285a70d8c8868fa1f740044195558ee
Jonas0304 <jsalattorres@gmail.com>
2020-11-13 10:41:57 +0100

    Update filtrar-covid.cpp

--- filtrar-covid.cpp
+++ filtrar-covid.cpp
@@ -1,15 +1,42 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
    int contador = 0;
-   char caracter;
+   char caracter, char1, char2, char3;
+   bool EsMun = false;
 
    scanf("%c", &caracter);
-   while( caracter != '#' ) {
-      contador = contador+1;
+   while( caracter != '#' ) 
+   {
+      if(caracter == ',')
+      {
+         contador = contador+1;
+         if (contador == 4)
+         {
+            scanf("%c", &char1);
+            scanf("%c", &char2);
+            scanf("%c", &char3);
+            if(char1 == 'L' && char2 == 'l' && char1 == 'e')
+            {
+               EsMun = true;
+
+            }
+         }
+         else if (contador == 9)
+         {
+            if (EsMun == true)
+            {
+               scanf("%c", &caracter);
+               printf("%c\n", caracter);
+            }
+            
+            contador = 0;
+            EsMun = false;
+         }
+      }
       scanf("%c", &caracter);
    }
-   printf("Resultat %i\n", contador);
-   printf("#");
+   
 }
