7109bdba05f3db1162ca3b2b71c31c8440877bed
bk203esd <bk203esd@gmail.com>
2020-11-13 11:04:55 +0100

    filtro covid

--- filtrar-covid.cpp
+++ filtrar-covid.cpp
@@ -3,33 +3,41 @@
 
 int main() 
 {
-   int contador = 0;
+   int contador = 0, cockvid;
    char caracter, char1, char2, char3;
    bool EsMun = false;
 
    scanf("%c", &caracter);
    while( caracter != '#' ) 
    {
+      //printf("entra while#\n");
       if(caracter == ',')
       {
+         //printf("ifComa\n");
          contador = contador+1;
          if (contador == 4)
          {
+            //printf("if4\n");
             scanf("%c", &char1);
+            //printf("%c\n", char1);
             scanf("%c", &char2);
+            //printf("%c\n", char2);
             scanf("%c", &char3);
-            if(char1 == 'L' && char2 == 'l' && char1 == 'e')
+            //printf("%c\n", char3);
+            if(char1 == 'L' && char2 == 'l' && char3 == 'e')
             {
+               //printf("iflleida\n");
                EsMun = true;
 
             }
          }
-         else if (contador == 9)
+         else if (contador == 8)
          {
             if (EsMun == true)
             {
-               scanf("%c", &caracter);
-               printf("%c\n", caracter);
+               //printf("entra ifMUN\n");
+               scanf("%i", &cockvid);
+               printf("%i\n", cockvid);
             }
             
             contador = 0;
@@ -38,5 +46,5 @@ int main()
       }
       scanf("%c", &caracter);
    }
-   
+      printf("-1\n");
 }
