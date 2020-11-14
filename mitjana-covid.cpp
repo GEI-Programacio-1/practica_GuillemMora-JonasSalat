ce42c778e47001e418eea111a00cee76aa31251e
Jonas0304 <jsalattorres@gmail.com>
2020-11-14 01:33:33 +0100

    Update mitjana-covid.cpp

--- mitjana-covid.cpp
+++ mitjana-covid.cpp
@@ -2,12 +2,20 @@
 #include <stdio.h>
 
 int main() {
-   int acumulador = 0, valor;
+   int acumulador = 0, valor, comptador = 0;
 
    scanf("%i", &valor);
-   while( valor != 0 ) {
+   while( valor != -1 ) 
+   {
       acumulador = acumulador + valor;
       scanf("%i", &valor);
+      comptador++ ;
+      if (comptador == 5)
+      {
+         printf("%i\n",acumulador/comptador);
+         acumulador = 0;
+         comptador = 0;
+      }
    }
-   printf("Resultat %i\n", acumulador);
+   printf("%i\n",acumulador/comptador);
 }
