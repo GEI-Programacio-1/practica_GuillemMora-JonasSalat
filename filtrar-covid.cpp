ca561b316b8b3fd74482b362bb76b6ba0227f5d4
bk203esd <bk203esd@gmail.com>
2020-11-27 08:43:58 +0100

    mod filtrar i mitjana

--- filtrar-covid.cpp
+++ filtrar-covid.cpp
@@ -3,47 +3,54 @@
 
 int main() 
 {
-   int contador = 0, cockvid;
-   char caracter, char1, char2, char3;
+   int contador = 0, covid;
+   char caracter, char1, char2, char3, campscorrectes;
    bool EsMun = false;
 
    scanf("%c", &caracter);
-   while( caracter != '#' ) 
-   {
-      //printf("entra while#\n");
-      if(caracter == ',')
-      {
-         //printf("ifComa\n");
-         contador = contador+1;
-         if (contador == 4)
-         {
-            //printf("if4\n");
-            scanf("%c", &char1);
-            //printf("%c\n", char1);
-            scanf("%c", &char2);
-            //printf("%c\n", char2);
-            scanf("%c", &char3);
-            //printf("%c\n", char3);
-            if(char1 == 'L' && char2 == 'l' && char3 == 'e')
-            {
-               //printf("iflleida\n");
-               EsMun = true;
+   
+   while( caracter != '#' ){
+      //printf("entra al bucle \n");
 
+      if(caracter == ','){
+         //printf("entra al condicional de la coma \n");
+
+         contador = contador+1;
+         
+		 if (contador == 4){
+            //printf("entra al condicional de la posició 4 \n");
+            
+			scanf("%c", &char1);
+            scanf("%c", &char2);
+            scanf("%c", &char3);
+            
+			if(char1 == 'L' && char2 == 'l' && char3 == 'e'){
+               //printf("entra al condicional dels caracters Lle \n");
+
+               EsMun = true;
             }
-         }
-         else if (contador == 8)
-         {
-            if (EsMun == true)
-            {
-               //printf("entra ifMUN\n");
-               scanf("%i", &cockvid);
-               printf("%i\n", cockvid);
+         }else if ((contador > 5) && (contador < 8)){
+			if (EsMun == true){
+               printf("%c", caracter);
+            }
+		 }	 
+         else if (contador == 8){
+            if (EsMun == true){
+               //printf("entra al condiciona de si es el Municipi \n");
+               
+			   printf("%c", caracter);
+			   scanf("%i", &covid);
+               printf("%i\n", covid);
             }
             
             contador = 0;
             EsMun = false;
          }
-      }
+      }else if ((contador > 4) && (contador < 8)){
+			if (EsMun == true){
+               printf("%c", caracter);
+            }
+		}
       scanf("%c", &caracter);
    }
       printf("-1\n");
--- mitjana-covid.cpp
+++ mitjana-covid.cpp
@@ -2,9 +2,34 @@
 #include <stdio.h>
 
 int main() {
-   int acumulador = 0, valor, comptador = 0;
+   int acumulador = 0, valor, comptador = 0, contador = 0;
+   char caracter;
+   bool final = false;
 
-   scanf("%i", &valor);
+   do{
+      scanf("%c", &caracter);
+      printf("%c", caracter);
+
+      if (caracter == ','){
+         contador++;
+         printf("%i", contador);
+
+         if (contador == 3){
+            scanf("%i", &valor);
+            acumulador = acumulador + valor;
+         }
+         
+      }else if (caracter == '-'){
+         scanf("%c", &caracter);
+         if (caracter == '1'){
+            final = true;
+         }
+         
+      }   
+   } while (!final);
+   
+
+   /* scanf("%i", &valor);
    while( valor != -1 ) 
    {
       acumulador = acumulador + valor;
@@ -16,6 +41,6 @@ int main() {
          acumulador = 0;
          comptador = 0;
       }
-   }
+   } */
    printf("%i\n",acumulador/comptador);
 }
